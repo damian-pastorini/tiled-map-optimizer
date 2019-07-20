2423d90f1a158fd4ae36aae4f915276f6a9c7ab5
Damian A. Pastorini <damian.pastorini>
2019-07-20 21:06:25 +0200

    - Tiledmap Optimized - v0.0.1

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
 <html>
 <head>
     <title>DwD - Tilemap Optimizer</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo OPTIMIZER_URL; ?>/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo OPTIMIZER_URL; ?>/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo OPTIMIZER_URL; ?>/favicon-16x16.png">
+    <link rel="manifest" href="<?php echo OPTIMIZER_URL; ?>/site.webmanifest">
     <link rel="stylesheet" href="<?php echo OPTIMIZER_URL; ?>/css/bootstrap.min.css"/>
     <script src="<?php echo OPTIMIZER_URL; ?>/js/jquery-3.4.1.min.js" type="text/javascript"></script>
     <script src="<?php echo OPTIMIZER_URL; ?>/js/bootstrap.bundle.min.js" type="text/javascript"></script>
